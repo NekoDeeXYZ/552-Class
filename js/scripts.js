ad1acd8cd445b1b31069a502fbf425c192f20ae7
Delany <134228543+NekoDeeXYZ@users.noreply.github.com>
2023-09-03 01:31:03 -0400

    Button that eats/throws up its friends.
    made it so that weekly selections are collapsable.
    Added some Horror content, including steam links and basic formating.
    made me hat emyself.
    am tired.

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,21 @@ const toggleButton = document.querySelector(".btn-toggle");
 toggleButton.addEventListener("click", function() {
     document.querySelector('html').toggleAttribute('data-dark-mode')
 });
+
+
+//ok now collapsable stuffs:
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++){
+    coll[i].addEventListener("click",function(){
+        this.classList.toggle("active");
+//NOW EAT AND BARF UP YOUR SIBLING MUHAHAHAHAHAHAHAHAHA
+        var content = this.nextElementSibling;
+        if (content.style.display === "block"){
+            content.style.display = "none";}
+            else {
+                content.style.display = "block";
+            }
+        });
+}
